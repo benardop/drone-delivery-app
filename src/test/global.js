24fc1b0afc2baad347b07f9f19310b580f1d08ba
benardop <benard.oc.p@gmail.com>
2023-03-02 20:56:38 +0300

    Update package.json file

--- src/test/global.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const path = require('path');
-
-let rootDir = path.join(__dirname, '../..');
-
-global.expect = require('chai').expect;
-
-global.APP_DIR = path.join(rootDir, 'src/app');
